8a32cd95c33e31f6ca8bdb91f0cec4e72e5079a9
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-06-05 00:04:24 +0530

    recursion questions added

---
+++ Questions/question_19.py
@@ -0,0 +1,6 @@
+f = int(input("Enter temperature in farhentie: "))
+
+c= 5 * (f -32) /9
+
+print(c)
+
---
+++ Questions/question_20.py
@@ -0,0 +1,8 @@
+def sum(a):
+    if(a == 1):
+        return 1
+    else:
+        return a + sum(a-1)
+    
+a = sum(10)
+print(a)
