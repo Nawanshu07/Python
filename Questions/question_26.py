bff8bf7e6346f3101c3c5e75aa0c71178aed4e84
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-06-11 23:01:52 +0530

    .

--- Questions/question_26.py
+++ Questions/question_26.py
@@ -1,7 +1,13 @@
 n = int(input("Enter the limit: "))
 
-for i in range (1 , n+1):
-    for j in range (1,10):
-        with open("file.txt" , "w") as f:
-            f.write(f"{i} x {j} = {i*j}")
+def gen_table(n):
+    table = ""
+    for i in range (1,11):
+        table+= f"{n} x {i} = {n*i}\n"
+    
+    with open(f"table/table_{n}.txt" , "w") as f:
+        f.write(table)
+
+for i in range (1,n+1):
+    gen_table(i)
             
