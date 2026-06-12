2361f5c2da7f68888228a06a1aea8a26102688d9
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-06-13 01:23:49 +0530

    Create question_27.py

---
+++ Questions/question_27.py
@@ -0,0 +1,9 @@
+class programmer:
+    company = "Microsoft"
+    def __init__(self , name , salary):
+        self.name = name
+        self.salary = salary
+    
+nawanshu = programmer("Nawanshu" , 12341)
+
+print(nawanshu.name , nawanshu.salary , nawanshu.company)
