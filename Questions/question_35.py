a80a244af727d95593fe683a337c44b30062b7d9
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-06-23 18:28:13 +0530

    Create question_35.py

---
+++ Questions/question_35.py
@@ -0,0 +1,5 @@
+l = ["apple" , "mango" , "kela" , "keli" , "angur"]
+
+for index , i in enumerate(l):
+    print(f"{index}. {i}")  #we can't use step jumping in enumerate but we can you 
+                            #if statement with index to jump 
