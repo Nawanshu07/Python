8ab5c7cdf4d9a0b8f398c0fa283da9532f9ea16c
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-08-13 22:41:56 +0530

    Create question_40.py

---
+++ Questions/question_40.py
@@ -0,0 +1,20 @@
+n = int(input("Enter the n:"))
+num = n-1
+for i in range(n):
+    for j in range (n-i):
+        print(" ",end="")
+
+    for k in range (i*2-1):
+        print("*" , end="")
+
+    print()
+
+for i in range(2,n):
+    for k in range(i):
+        print(" ", end="")
+
+    for k in range((n-i)*2-1):
+        print("*", end="")
+
+    print()
+    
