a2e05628d1b317b7a656fed6a07ee7049439b04e
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-08-15 01:51:20 +0530

    started tkinter

---
+++ Tkinter/first.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("My first Tkinter app")
+
+label = tk.Label(root,text="Hello , Tkinter!!")
+label.pack()
+
+root.geometry("1920x1080")
+
+icon = tk.PhotoImage(file = 'image.png')
+root.iconphoto(True , icon)
+root.config(background="black")
+
+root.mainloop()
